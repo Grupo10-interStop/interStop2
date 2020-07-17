7a82fe2f8bb6ba826122cbeddabcc8039d164fde
Ronaldo <ronaldo1103mtz@gmail.com>
2020-07-16 22:00:23 -0600

    app inicio y administrar2

--- interStop/settings.py
+++ interStop/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'interStop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'interStop',
         'USER': 'interStop',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
-        'DATEBASE_PORT': '5432',
+        'PORT': '5433',
     }
 }
 
